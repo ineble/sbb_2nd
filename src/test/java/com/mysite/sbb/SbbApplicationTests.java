96d8d05193e2328067f17b085cbc655518c4c53c
ineble <thdwltjq94@gmail.com>
2022-04-24 17:54:24 +0900

    2022-04-24

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,9 +1,14 @@
 package com.mysite.sbb;
 
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.answer.AnswerRepository;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +23,24 @@ class SbbApplicationTests {
 
     @Autowired
     private AnswerRepository answerRepository;
+
+    @Test
+    @Transactional
+    void testFindRelatedAnswers() {
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        List<Answer> answerList = q.getAnswerList();
+        assertEquals(1,answerList.size());
+        assertEquals("네 자동으로 생성됩니다.",answerList.get(0).getContent());
+    }
+    @Test
+    void testQuestionFind(){
+        Optional<Answer> oa = this.answerRepository.findById(1);
+        assertTrue(oa.isPresent());
+        Answer a = oa.get();
+        assertEquals(2,a.getQuestion().getId());
+    }
 
     @Test
     void testCreateAnswer() {
