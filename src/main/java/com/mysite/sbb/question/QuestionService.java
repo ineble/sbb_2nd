4dc98efdea3badbc351475720c029ef27d4d2c37
ineble <thdwltjq94@gmail.com>
2022-04-30 17:10:09 +0900

    paging

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -2,9 +2,14 @@ package com.mysite.sbb.question;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,4 +38,11 @@ public class QuestionService {
         q.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q);
     }
+    public Page<Question> getList(int page){
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.asc("createDate"));
+        Pageable pageable = PageRequest.of(page,10);
+        return  this.questionRepository.findAll(pageable);
+    }
+
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -4,6 +4,7 @@ import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.answer.AnswerRepository;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -137,5 +138,17 @@ class SbbApplicationTests {
     @Test
     void contextLoads() {
     }
+    @Autowired
+    private QuestionService questionService;
+
+    @Test
+    void testJpa(){
+        for(int i = 1; i <= 300; i ++){
+            String subject = String.format("테스트 데이터입니다:[%03d]",i);
+            String content = "내용무";
+            this.questionService.create(subject,content);
+        }
+    }
+
 
 }
